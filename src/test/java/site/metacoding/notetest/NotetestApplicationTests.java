28047aa7c1d79110e1db6fbdefd9c5961626fa62
a01048197443 <a01048197443@gmail.com>
2022-05-18 17:54:04 +0900

    프로젝트 세팅

---
+++ src/test/java/site/metacoding/notetest/NotetestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.notetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
